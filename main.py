046b1cae35241630ca7357b59c198a5c17a95b85
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-07-13 14:56:43 -0300

    method by download file s3

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from src.drivers.storage import Storage
+import logging
 
 # entry point
 if __name__ == '__main__':
 
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
     # connect storage by s3
     storage = Storage()
 
+    bucket = 'testsdataeng'
+    path_storage = 'tcemg/2022.zip'
+    path_local = 'tmp/2022.zip'
+
     # get file
-    file_zip = storage.get_object('testsdataeng', 'tcemg/22.zip')
+    file_zip = storage.get_object(bucket, path_storage)
 
     # if not file
     if not file_zip:
-        print('file not exists')
+        logging.info('Arquivo ' + path_storage + 'não encotrado no bucket')
+    else:
+
+        is_file_local = storage.is_file_local(path_local)
+
+        if not is_file_local:
+            logging.info('Baixando novo arquivo')
+            file = storage.download_file(bucket, path_storage, path_local)
+        else:
+            logging.info('Arquivo .zip ja exite')
--- src/drivers/storage.py
+++ src/drivers/storage.py
@@ -1,22 +1,31 @@
 import os
 from typing import Dict, Any
-
 import boto3
 from dotenv import load_dotenv
+import logging
 
 from src.drivers.interfaces.storage_interface import StorageInterface
 
 
 class Storage(StorageInterface):
     __storage: boto3
+    __path: str
 
     def __init__(self) -> None:
+
+        logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
         # load envs
         load_dotenv()
 
         self.__storage = self.connect()
 
+        self.__path = os.path.dirname(os.path.realpath(__file__))
+
     def connect(self) -> boto3:
+
+        logging.info('Conecta no bucket')
+
         service = os.getenv("STORAGE")
         region_name = os.getenv("AWS_DEFAULT_REGION")
         aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
@@ -41,3 +50,21 @@ class Storage(StorageInterface):
 
         except Exception:
             return False
+
+    def download_file(self, bucket, key, path) -> Any:
+        try:
+            path = os.path.join(self.__path, path)
+            object_file = self.__storage.download_file(Bucket=bucket, Key=key, Filename=path)
+
+            return object_file
+
+        except Exception:
+            return False
+
+    def is_file_local(self, path)-> bool:
+
+        path_full = os.path.join(self.__path, path)
+
+        check_file = os.path.isfile(path_full)
+
+        return check_file
