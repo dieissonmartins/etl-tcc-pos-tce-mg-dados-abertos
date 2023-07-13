7d09f323995f529c842baa981cc6864f16e51b43
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-07-13 11:37:47 -0300

    debug

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from src.drivers.storage import Storage
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# connect storage by s3
+storage = Storage()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Dieisson Martins')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bukects = storage.list_buckets()
