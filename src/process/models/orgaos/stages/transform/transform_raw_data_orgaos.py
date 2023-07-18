b917953dab32a4f659d6b056bab50c15c782b9e0
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-07-18 11:38:53 -0300

    start transform data all fields

--- src/process/models/orgaos/stages/transform/transform_raw_data_orgaos.py
+++ src/process/models/orgaos/stages/transform/transform_raw_data_orgaos.py
@@ -10,19 +10,36 @@ class TransformRawDataOrgaos:
         aux = []
 
         for row in data_content:
-
             seq_orgao = int(row['seq_orgao'])
             num_anoexercicio = int(row['num_anoexercicio'])
-            cod_orgao = int(row['cod_orgao'])
+            cod_orgao = str(row['cod_orgao'])
+            nom_orgao = str(row['nom_orgao'])
+            tipo_orgao = str(row['tipo_orgao'])
+            cod_municipio = int(row['cod_municipio'])
+            nom_municipio = str(row['nom_municipio'])
+            cod_uf = str(row['cod_uf'])
+            sgl_uf = str(row['sgl_uf'])
+            nom_uf = str(row['nom_uf'])
+            dsc_regiaoplanejamento = str(row['dsc_regiaoplanejamento'])
+            num_versao_arq = str(row['num_versao_arq'])
 
             row = {
                 'seq_orgao': seq_orgao,
                 'num_anoexercicio': num_anoexercicio,
                 'cod_orgao': cod_orgao,
+                'nom_orgao': nom_orgao,
+                'tipo_orgao': tipo_orgao,
+                'cod_municipio': cod_municipio,
+                'nom_municipio': nom_municipio,
+                'cod_uf': cod_uf,
+                'sgl_uf': sgl_uf,
+                'nom_uf': nom_uf,
+                'dsc_regiaoplanejamento': dsc_regiaoplanejamento,
+                'num_versao_arq': num_versao_arq
             }
 
             aux.append(row)
 
         ret = aux
 
-        return []
+        return ret
