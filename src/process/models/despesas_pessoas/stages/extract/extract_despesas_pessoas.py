eb47c4dbfb84f2e1e9e2911aaf9f87c2fb2b49bb
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-07-25 21:20:13 -0300

    extract despesas pessoas

---
+++ src/process/models/despesas_pessoas/stages/extract/extract_despesas_pessoas.py
@@ -0,0 +1,42 @@
+import os
+from typing import Dict
+import pandas as pd
+import logging
+
+
+class ExtractDespesasPessoas:
+
+    def __init__(self, path, file_path, model, entity_id) -> None:
+        self.__path = path
+        self.__file_path = file_path
+        self.__model = model
+        self.__entity_id = entity_id
+        self.__file = self.__path + '/src/drivers/tmp/' + self.__file_path
+
+    def extract(self) -> Dict:
+        logging.info('Extrai despesas pessoas')
+
+        file = self.__file
+
+        check_file = os.path.isfile(file)
+
+        if not check_file:
+            return {}
+
+        col_names = [
+            'cod_municipio',
+            'num_ano_referencia',
+            'num_mes_referencia',
+            'cod_nat_despesa',
+            'dsc_nat_despesa',
+            'vlr_executivo',
+            'vlr_legislativo',
+            'vlr_municipio',
+            'num_versaoarq'
+        ]
+
+        df = pd.read_csv(file, header=0, names=col_names, sep=';')
+
+        rows = df.to_dict(orient='records')
+
+        return rows
